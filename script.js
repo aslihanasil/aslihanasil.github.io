877637ea87888ce532cab293c11a86a5f6b17acb
Aslihan Asil <aslihanasil@Aslihans-MacBook-Pro-2.local>
2023-09-04 10:55:02 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function f1() {
+    let x = document.getElementById("abstract-1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function f2() {
+    let x = document.getElementById("abstract-2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function f3() {
+    let x = document.getElementById("abstract-3");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function f4() {
+    let x = document.getElementById("abstract-4");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
